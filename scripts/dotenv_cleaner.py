3df8e8920c02b42790ed9586ca9cfe2b9cea64f5
Odessit007 <igor.vustyanyuk@gmail.com>
2023-08-06 00:04:18 +0300

    Update dotenv stuff

--- scripts/dotenv_cleaner.py
+++ scripts/dotenv_cleaner.py
@@ -10,17 +10,28 @@ import dotenv
 from pathlib import Path
 
 
-keys_to_keep_vals = ['HPO_OBO_PATH', 'LOCAL_ARTIFACTS_DIR', 'MODE', 'PREFECT_API_URL', 'REMOTE_ARTIFACTS_DIR']
+keys_to_keep_vals = [
+    'AWS_BLOCK_NAME', 'HPO_OBO_PATH', 'LOCAL_ARTIFACTS_DIR', 'MODE', 'N_SWEEP_RUNS', 'NUMPY_RANDOM_SEED',
+    'PREFECT_API_ENABLE_HTTP2', 'PREFECT_API_URL', 'PREFECT_LOGGING_EXTRA_LOGGERS', 'PREFECT_LOGGING_LEVEL',
+    'PYTHONHASHSEED', 'REMOTE_ARTIFACTS_DIR', 'WANDB_BASE_URL', 'WANDB_MODE'
+]
 config_root_dir = Path('config/')
-files_with_credentials = ['.env.aws', '.env.cloud']
+files_with_credentials = ['.env.aws', '.env.cloud', '.env.local']
 for file in files_with_credentials:
     input_path = config_root_dir / file
     output_path = input_path.with_name(file + '.template')
     print(f'Processing {input_path}. Writing to {output_path}.')
-    config = dotenv.dotenv_values(input_path)
+    with input_path.open() as fin:
+        config = {}
+        for line in fin:
+            line = line.strip().removeprefix('export ')
+            if not line:
+                continue
+            key, val = line.split('=', maxsplit=1)
+            config[key] = val
     with output_path.open('w') as fout:
-        for key, val in config.items():
-            if key in keys_to_keep_vals:
-                fout.write(f'{key}={val}\n')
+        for key in sorted(config):
+            if key in keys_to_keep_vals or key.removeprefix('# ') in keys_to_keep_vals:
+                fout.write(f'{key}={config[key]}\n')
             else:
-                fout.write(f'{key}=\n')
+                fout.write(f'{key}={"?" * 50}\n')
