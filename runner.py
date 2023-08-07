a00394c7bb5cbebb34ece03402eed64add1b7368
Odessit007 <igor.vustyanyuk@gmail.com>
2023-08-08 02:02:35 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -9,6 +9,7 @@ logger.addHandler(logging.StreamHandler())
 
 MODE = 'cloud'
 dotenv.load_dotenv(f'config/.env.{MODE}', verbose=True, override=True)
+dotenv.load_dotenv(f'config/.env.aws', verbose=True, override=True)
 
 
 if __name__ == '__main__':
@@ -16,6 +17,12 @@ if __name__ == '__main__':
     from hpw.data_preprocessing import prepare_data
     logger.info(f'PREFECT_API_URL = {os.getenv("PREFECT_API_URL")}')
     # prepare_data(force_download=True, verify_ssl=False)
-    from hpw.training import train_model, tune
-    # train_model('word2vec', {})
+    from hpw.training import model_flow, tune
     tune()
+    # _, score = model_flow('eval', dict(
+    #     model_name='word2vec',
+    #     use_stop_words=False,
+    #     split_into_subwords=True,
+    #     model_artifact_name='best_model',
+    # ))
+    # print(score)
